1a1eeb8dc8a9752d1baa7ad8ca60d10d8a852d68
esetaro <eliza96@vt.edu>
2017-04-04 13:01:11 -0400

    image audience

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -2,8 +2,9 @@
 fl = new Array();
 fl["0"]= "t-frappe.html";
 fl["1"]= "t-oj.html";
-fl["2"]= "t-vhash.html";
-fl["3"]= "t-vomelet.html";
-fl["4"]= "t-vsandwich.html";
-fl["5"]= "t-vwaffles.html";
+fl["2"]= "t-sandwich.html";
+fl["3"]= "t-vhash.html";
+fl["4"]= "t-vomelet.html";
+fl["5"]= "t-vsandwich.html";
+fl["6"]= "t-vwaffles.html";
 var doStem = false;searchLoaded = true;
