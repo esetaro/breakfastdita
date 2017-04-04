dd41798dac7ed3503ffb8053bc2676408a2f1488
esetaro <eliza96@vt.edu>
2017-04-04 13:41:36 -0400

    so many changes so little time

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,8 +1,9 @@
 fil = new Array();
 fil["0"]= "t-frappe.html@@@How to Make a Caramel Frappe@@@This delightful drink will certainly wake you up!...";
 fil["1"]= "t-oj.html@@@How to Make Freshly-Squeezed Orange Juice@@@This bright citrus drink will awaken your senses and propell you into the start of your day!...";
-fil["2"]= "t-sandwich.html@@@How to Make a Breakfast Sandwich@@@A breakfast is both easy to make and filling...";
-fil["3"]= "t-vhash.html@@@How to Make a Kale and Tempeh Breakfast Hash@@@Wants something filling but healthy? This Vegan Breakfast Hash will certainly do the job...";
-fil["4"]= "t-vomelet.html@@@How to Make a Tofu Veggie Omelet@@@This omelet definitely packs a punch. With no meat, but plenty of flavor, this is a great way to begin your day...";
-fil["5"]= "t-vsandwich.html@@@How to Make a Tempeh Bacon Breakfast Sandwich@@@A breakfast is both easy to make and filling...";
-fil["6"]= "t-vwaffles.html@@@How to Make Blueberry Oatmeal Waffles@@@Waffles are an excellent way to start your day. These delicious Blueberry Waffles are both healthy and easy to make...";
+fil["2"]= "t-omelet.html@@@How to Make an Omelet@@@Omelets are delicious! Using this recipe you can create something to make for both your friends and family!...";
+fil["3"]= "t-sandwich.html@@@How to Make a Breakfast Sandwich@@@A breakfast is both easy to make and filling...";
+fil["4"]= "t-vhash.html@@@How to Make a Kale and Tempeh Breakfast Hash@@@Wants something filling but healthy? This Vegan Breakfast Hash will certainly do the job...";
+fil["5"]= "t-vomelet.html@@@How to Make a Tofu Veggie Omelet@@@This omelet definitely packs a punch. With no meat, but plenty of flavor, this is a great way to begin your day...";
+fil["6"]= "t-vsandwich.html@@@How to Make a Tempeh Bacon Breakfast Sandwich@@@A breakfast is both easy to make and filling...";
+fil["7"]= "t-vwaffles.html@@@How to Make Blueberry Oatmeal Waffles@@@Waffles are an excellent way to start your day. These delicious Blueberry Waffles are both healthy and easy to make...";
--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -2,9 +2,10 @@
 fl = new Array();
 fl["0"]= "t-frappe.html";
 fl["1"]= "t-oj.html";
-fl["2"]= "t-sandwich.html";
-fl["3"]= "t-vhash.html";
-fl["4"]= "t-vomelet.html";
-fl["5"]= "t-vsandwich.html";
-fl["6"]= "t-vwaffles.html";
+fl["2"]= "t-omelet.html";
+fl["3"]= "t-sandwich.html";
+fl["4"]= "t-vhash.html";
+fl["5"]= "t-vomelet.html";
+fl["6"]= "t-vsandwich.html";
+fl["7"]= "t-vwaffles.html";
 var doStem = false;searchLoaded = true;
